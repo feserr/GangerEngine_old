3bbc73feda85b65b3c4fd0a2148bd90cba0cd67c
Reisor <fraserm1989@gmail.com>
2015-05-19 21:21:21 +0200

    Update the engine till video 22 part 2

--- src/SpriteBatch.cpp
+++ src/SpriteBatch.cpp
@@ -22,11 +22,20 @@ namespace GangerEngine
     void SpriteBatch::Begin(GlyphSortType glyphSortType /* GlyphShortType::TEXTURE*/)
     {
         _glyphSortType = glyphSortType;
+        _renderBatches.clear();
+
+        for(unsigned int i = 0; i < _glyphs.size(); i++)
+        {
+            delete _glyphs[i];
+        }
+
+        _glyphs.clear();
     }
     
     void SpriteBatch::End()
     {
         SortGlyphs();
+        CreateRenderBatches();
     }
 
     void SpriteBatch::Draw(const glm::vec4& destRect, const glm::vec4& uvRect,
@@ -37,7 +46,7 @@ namespace GangerEngine
         glyph->depth = depth;
 
         
-        glyph->topLeft.SetPosition(destRect.x, destRect.y + uvRect.w);
+        glyph->topLeft.SetPosition(destRect.x, destRect.y + destRect.w);
         glyph->topLeft.SetUV(uvRect.x, uvRect.y + uvRect.w);
         glyph->topLeft.color = color;
 
@@ -45,19 +54,93 @@ namespace GangerEngine
         glyph->bottonLeft.SetUV(uvRect.x, uvRect.y);
         glyph->bottonLeft.color = color;
 
-        glyph->topRight.SetPosition(destRect.x + uvRect.z, destRect.y + uvRect.w);
-        glyph->topRight.SetUV(uvRect.x + uvRect.z, uvRect.y + uvRect.w);
-        glyph->topRight.color = color;
-
-        glyph->bottonRight.SetPosition(destRect.x + uvRect.z, destRect.y);
+        glyph->bottonRight.SetPosition(destRect.x + destRect.z, destRect.y);
         glyph->bottonRight.SetUV(uvRect.x + uvRect.z, uvRect.y);
         glyph->bottonRight.color = color;
+
+        glyph->topRight.SetPosition(destRect.x + destRect.z, destRect.y + destRect.w);
+        glyph->topRight.SetUV(uvRect.x + uvRect.z, uvRect.y + uvRect.w);
+        glyph->topRight.color = color;
 
         _glyphs.push_back(glyph);
     }
 
     void SpriteBatch::RenderBatch()
     {
+        // Bind our VAO. This sets up the OpenGL state we need, including the 
+        // vertex attribute pointers and it binds the VBO
+        glBindVertexArray(_vao);
+
+        for(unsigned int i = 0; i < _renderBatches.size(); i++) {
+            glBindTexture(GL_TEXTURE_2D, _renderBatches[i].texture);
+
+            glDrawArrays(GL_TRIANGLES, _renderBatches[i].offset, _renderBatches[i].numVertices);
+        }
+
+        glBindVertexArray(0);
+    }
+
+    void SpriteBatch::CreateRenderBatches()
+    {
+        if(_glyphs.empty())
+            return;
+
+        // This will store all the vertices's that we need to upload
+        std::vector<Vertex> vertices;
+
+        // Allocate all the memory we need for the vertices's we will add
+        vertices.resize(_glyphs.size() * 6);
+
+        int offset = 0;
+        int currentVertex = 0;
+
+        // Create a new object and push it back
+        _renderBatches.emplace_back(offset, 6, _glyphs[0]->texture);
+
+        // Add the first batch
+        vertices[currentVertex++] = _glyphs[0]->topLeft;
+        vertices[currentVertex++] = _glyphs[0]->bottonLeft;
+        vertices[currentVertex++] = _glyphs[0]->bottonRight;
+        vertices[currentVertex++] = _glyphs[0]->bottonRight;
+        vertices[currentVertex++] = _glyphs[0]->topRight;
+        vertices[currentVertex++] = _glyphs[0]->topLeft;
+        
+        offset += 6;
+
+        // Add all the rest of the glyphs
+        for(unsigned int currentGlyph = 1; currentGlyph < _glyphs.size(); currentGlyph++)
+        {
+            // Check if the current glyph can be part of the current batch
+            if(_glyphs[currentGlyph]->texture != _glyphs[currentGlyph - 1]->texture)
+            {
+                // Create a new batch
+                _renderBatches.emplace_back(offset, 6, _glyphs[currentGlyph]->texture);
+            }
+            else
+            {
+                // If it is part of the current batch then increase the numVertices
+                _renderBatches.back().numVertices += 6;
+            }
+
+            vertices[currentVertex++] = _glyphs[currentGlyph]->topLeft;
+            vertices[currentVertex++] = _glyphs[currentGlyph]->bottonLeft;
+            vertices[currentVertex++] = _glyphs[currentGlyph]->bottonRight;
+            vertices[currentVertex++] = _glyphs[currentGlyph]->bottonRight;
+            vertices[currentVertex++] = _glyphs[currentGlyph]->topRight;
+            vertices[currentVertex++] = _glyphs[currentGlyph]->topLeft;
+
+            offset += 6;
+        }
+
+        // Bind our VBO
+        glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+        // Orphan the buffer (for speed) because it overwrite the current data
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), nullptr, GL_DYNAMIC_DRAW);
+        // Upload the data
+        glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size() * sizeof(Vertex), vertices.data());
+
+        // Unbind the VBO
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
     }
 
     void SpriteBatch::CreateVertexArray()
@@ -115,16 +198,16 @@ namespace GangerEngine
 
     bool SpriteBatch::CompareBackToFront(Glyph* a, Glyph* b)
     {
-        return a->depth > b->depth;
+        return (a->depth > b->depth);
     }
 
     bool SpriteBatch::CompareFrontToBack(Glyph* a, Glyph* b)
     {
-        return a->depth < b->depth;
+        return (a->depth < b->depth);
     }
 
     bool SpriteBatch::CompareTexture(Glyph* a, Glyph* b)
     {
-        return a->texture < b->texture;
+        return (a->texture < b->texture);
     }
 }
